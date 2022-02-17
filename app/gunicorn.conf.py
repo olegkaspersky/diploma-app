dfa55607fb10801b581b057adb91824bff383e7e
Oleg Kaspersky <kasperskiyoleg@yandex.ru>
2022-02-17 13:53:51 +0300

    testing gunicorn configurations
    testing gunicorn configurations 2

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -1,9 +1,17 @@
 import multiprocessing
 
+pidfile = 'nhl-app.pid'
+proc_name = 'nhl-app'
+
 bind = '0.0.0.0:5000'
+
 workers = (2 * multiprocessing.cpu_count()) + 1
-worker_class = 'gthread'
 threads = 2 * multiprocessing.cpu_count()
+worker_class = 'gthread'
+worker_tmp_dir = '/dev/shm'
+
+timeout = 30
+keepalive = 5
 
 
 # logging config section
