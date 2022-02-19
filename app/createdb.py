b6d4ed6a12c9ee8838ebc808e2e1fae5608c2dd8
Oleg Kaspersky <kasperskiyoleg@yandex.ru>
2022-02-19 12:46:56 +0300

    updated helpers

--- app/createdb.py
+++ app/createdb.py
@@ -2,20 +2,12 @@ import db
 import helpers
 
 years = [2021, 2020, 2019, 2018, 2017]
-game_types = ['final', 'allstar']
+game_types = ['P', 'A']
 
 for game_type in game_types:
     for year in years:
-        if game_type == game_types[0]:
-            game_info = helpers.get_game_data(helpers.get_season_final(year))
-
+        if year == 2021 and game_type == 'A':
+            continue
+        else: 
+            game_info = helpers.get_final_game_data(year, game_type)
             db.insert_data(game_type, year, game_info)
-
-        elif game_type == game_types[1]:
-            if year == 2021:
-                continue
-            else:
-                game_info = helpers.get_game_data(
-                    helpers.get_season_allstar_final(year))
-
-                db.insert_data(game_type, year, game_info)
